540bf3ad60bcbe83e78d17e87e077c153315586b
Travis Olmstead <travis@Traviss-MacBook-Pro.local>
2022-04-27 18:58:04 -0600

    Project created

---
+++ app/resources/js/Pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LandingPage() {
+    return (
+        <>
+            <div>
+                <button className="btn btn-primary">click me!</button>
+            </div>
+        </>
+    );
+}
